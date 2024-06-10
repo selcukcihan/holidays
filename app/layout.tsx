129411308651c648f54d49e89b6555557108be89
Selcuk Cihan <selcukcihan@gmail.com>
2024-06-10 21:15:59 +0300

    Upgrade next and enable google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css"
 import { getHolidays } from "../data"
 
@@ -58,6 +59,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-7KT40Q6DHY" />
     </html>
   )
 }
